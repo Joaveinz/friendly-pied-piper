46af37231e9618f2c6e4fad44315c7a41065716a
julo-web-dev <julianstaiger98@gmail.com>
2024-06-16 15:05:17 +0200

    restruktured project, added character facing direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+const ANGULAR_ACCELERATION = 10
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -11,16 +13,16 @@ var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
 
-@onready var twist_pivot := $TwistPivot
+@onready var twist_pivot := get_node("TwistPivot")
 @onready var pitch_pivot := $TwistPivot/PitchPivot
 
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-	
 func _process(delta: float) -> void:
 	pass
+	
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -37,15 +39,14 @@ func _physics_process(delta):
 	var direction = (twist_pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED         
+		velocity.z = direction.z * SPEED      
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
 	
-	if Input.is_action_just_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$Skeleton3D.rotation.y = lerp_angle($Skeleton3D.rotation.y, atan2(velocity.x,velocity.z), delta * ANGULAR_ACCELERATION)
+		
+	move_and_slide()
 		
 	twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(twist_input)
@@ -55,6 +56,10 @@ func _physics_process(delta):
 	)
 	twist_input = 0
 	pitch_input = 0
+	
+func _input(event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
