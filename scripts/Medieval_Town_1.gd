8de993238774938f304da77d7a1e1ae280ac66a9
julo-web-dev <julianstaiger98@gmail.com>
2024-06-22 18:57:18 +0200

    detect if all rats are killed and game is won

--- scripts/Medieval_Town_1.gd
+++ scripts/Medieval_Town_1.gd
@@ -1,15 +1,21 @@
 extends Node3D
 
+var îs_game_won : bool = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	
+	if get_tree().get_nodes_in_group("rats").size() == 0:
+		îs_game_won = true
+		
+	if îs_game_won:
+		print("Congratulation, you killed all rats")
 
 func _on_death_area_body_entered(body):
 	if body.name.contains("Rat"):
 		body.is_alive = false
+		body.queue_free()
+		
 		$UserInterface/ScoreLabel.increase_score()
-
 
 
 func _on_trash_area_body_entered(body):
