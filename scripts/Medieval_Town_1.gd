9feadc2d34ce62eaaf05815635c096f84336f7e2
julo-web-dev <julianstaiger98@gmail.com>
2024-06-22 18:00:54 +0200

    wip: create area around trash bin, goal ist for the rats to get there

--- scripts/Medieval_Town_1.gd
+++ scripts/Medieval_Town_1.gd
@@ -10,3 +10,9 @@ func _on_death_area_body_entered(body):
 		body.is_alive = false
 		$UserInterface/ScoreLabel.increase_score()
 
+
+
+func _on_trash_area_body_entered(body):
+	if body.name.contains("Rat"):
+		body.is_inside_trash_area = true
+		print("rat is inside trash area")
