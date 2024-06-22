5fd0a9adbed56347d32d4d4ebf0a3e6fdd39ec97
julo-web-dev <julianstaiger98@gmail.com>
2024-06-22 14:46:53 +0200

    Add DeathAreas around the map, kill rats when they entre area

--- scripts/Medieval_Town_1.gd
+++ scripts/Medieval_Town_1.gd
@@ -1,9 +1,10 @@
 extends Node3D
 
-@onready var player = $Player
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#get_tree().call_group("rats", "update_target_location", player.global_transform.origin)
 	pass
 	
+
+func _on_death_area_body_entered(body):
+	if body.name.contains("Rat"):
+		body.is_alive = false
